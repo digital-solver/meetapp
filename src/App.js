dd36ce6c01da3e1b30a89a6958a4dd3fc4c0db12
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-10-02 14:29:33 +0200

    Read event data from api and style app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { mockData } from "./mock-data";
+import { extractLocations, getEvents } from './api';
 
-function App() {
+class App extends Component {
+  state = {
+    events: mockData,
+    locations: extractLocations(mockData),
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') 
+      ? events 
+      : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+  
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  render() {
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
       <NumberOfEvents />
-      <EventList events={mockData}/>
+      <EventList events={this.state.events}/>
     </div>
   );
+}
 }
 
 export default App;
