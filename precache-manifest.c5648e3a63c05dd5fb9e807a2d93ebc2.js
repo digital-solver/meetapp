234dda5bf89487ec86f05b1c2a2d2c9787672763
Kerr <kerr.digitalsolver@gmail.com>
2022-10-03 10:46:45 +0200

    Updates

---
+++ precache-manifest.c5648e3a63c05dd5fb9e807a2d93ebc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb3bea9c4846d1cb99c6",
+    "url": "/meetapp/static/css/main.156401c5.chunk.css"
+  },
+  {
+    "revision": "fb3bea9c4846d1cb99c6",
+    "url": "/meetapp/static/js/main.fb3bea9c.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/meetapp/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "e6133865f8bd72c40a00",
+    "url": "/meetapp/static/js/2.e6133865.chunk.js"
+  },
+  {
+    "revision": "7b36c975574bf4c6593b",
+    "url": "/meetapp/static/js/runtime~main.7b36c975.js"
+  },
+  {
+    "revision": "2b52abfd6ac900674340069cc14f668b",
+    "url": "/meetapp/index.html"
+  }
+];
