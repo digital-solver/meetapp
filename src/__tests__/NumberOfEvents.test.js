2f8150131b6e3324cb4bfbd86f9a0190643153ac
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-10-19 12:44:53 +0200

    Remove redundant test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -11,13 +11,4 @@ describe('<NumberOfEvents /> component', () => {
   test('render number input', () => {
     expect(NumberOfEventsWrapper.find('.event-number')).toHaveLength(1);
   });
-
-  test('input value reflects state', () => {
-    NumberOfEventsWrapper.setState({
-      numberOfEvents: 32
-    });
-    const Event = { target: { value: 6 } };
-    NumberOfEventsWrapper.find('.event-number').simulate('change', Event);
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(6);
-  });
 });
