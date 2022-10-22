539308cb581e2206033f75c4ddb0adb2c487e58e
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-10-22 11:49:02 +0100

    Test: E2E: show/hide details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,16 +1,40 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an events details', () => {
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    jest.setTimeout(30000)
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
-    
-    const eventDetails = await page.$('.event .details__inner-container');
+  });
 
-    expect(eventDetails).toEqual({});
+  afterAll(async () => {
     browser.close();
   })
+
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .details__inner-container');
+    let eventDetailsClasses = await (await eventDetails.getProperty('classList')).jsonValue();
+    expect(Object.values(eventDetailsClasses)).not.toEqual(expect.objectContaining({"1": "visible"}));
+  });
+
+  test('User can expand an event to see its details', async () => {
+    await page.click('.event .details__toggle');
+    await page.waitForSelector('.event .visible')
+    const eventDetails = await page.$('.event .details__inner-container');
+    let eventDetailsClasses = await (await eventDetails.getProperty('classList')).jsonValue();
+    expect(Object.values(eventDetailsClasses)).toEqual(expect.objectContaining({"1": "visible"}));
+  });
+
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.event .details__toggle');
+    const eventDetails = await page.$('.event .details__inner-container');
+    let eventDetailsClasses = await (await eventDetails.getProperty('classList')).jsonValue();
+    expect(Object.values(eventDetailsClasses)).not.toEqual(expect.objectContaining({"1": "visible"}));
+  })
+
 });
