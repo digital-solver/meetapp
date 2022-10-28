fd644dad814f4fc1027e69a1e0ebdc7bc26fccfb
Kerr <kerr.digitalsolver@gmail.com>
2022-10-28 17:01:27 +0100

    Updates

--- precache-manifest.aec94d4ad6ca1b2878a4389678493ce1.js
+++ precache-manifest.aec94d4ad6ca1b2878a4389678493ce1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f18b3e4048f7bba57592",
-    "url": "/meetapp/static/css/main.eabb1081.chunk.css"
+    "revision": "837052730830b6036f71",
+    "url": "/meetapp/static/css/main.d2028535.chunk.css"
   },
   {
-    "revision": "f18b3e4048f7bba57592",
-    "url": "/meetapp/static/js/main.f18b3e40.chunk.js"
+    "revision": "837052730830b6036f71",
+    "url": "/meetapp/static/js/main.83705273.chunk.js"
   },
   {
     "revision": "2b447e1be3e7dd9deafc",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/meetapp/static/js/runtime~main.c5828c71.js"
   },
   {
-    "revision": "1c4235f17ff3a073ccd3c15aa01f2f64",
+    "revision": "d38f63e1cd15860ba0865f5d4ee8facc",
     "url": "/meetapp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meetapp/precache-manifest.2bbcb24f114c5b5859f03a5e1d5af3d0.js"
+  "/meetapp/precache-manifest.aec94d4ad6ca1b2878a4389678493ce1.js"
 );
 
 workbox.clientsClaim();
