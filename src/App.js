720075af71345755965b17c063c4d426fb120e15
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-11-07 19:02:48 +0000

    Typo

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
     const {locations, events} = this.state;
     
     const data = locations.map((location) => {
-      const number = events.filter((event) => event.location === location).eventsLength
+      const number = events.filter((event) => event.location === location).length
       const city = location.split(', ').shift()
       return {city, number};
     })
